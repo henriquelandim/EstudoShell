2de9d3375be25c4116d64543613ec67d91d62316
landim <henriquelandim@outlook.com.br>
2017-10-23 12:19:27 -0200

    Primeiros comandos em Shell

---
+++ calculos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#program para calcular dois numeros e fazer as quatros opecoes 
+clear 
+echo "----------------------------------------------"
+echo "-                                            -"
+echo "-      Programa de Calculos: 4 operacoes     -"
+echo "-                                            -"
+echo "----------------------------------------------"
+echo  ""
+echo "Digite um numero"
+read n1
+echo "Digite outro numero"
+read n2
+echo "O resultado da soma eh: $((n1+n2))"
+echo "O resultado da subtracao eh: $((n1-n2))"
+echo "O resultado da mutiplio eh: $((n1*n2))"
+echo "O resultado da divisao eh: $((n1/n2))"
+echo "O calculo foi realizado: `./tempo.sh`"
+echo "voe eh burro" 
+
---
+++ nomesobrenome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#programa que pede nome e sobrenome
+clear
+echo "programa de boas vindas ao usuario"
+echo "-----------------------------------------------------------"
+echo "Digite o seu primeiro nome"
+read nome
+echo "Agora digite o seu sobrenome"
+read sobrenome
+echo "Ola, Sr(a) $nome $sobrenome. Hoje eh: `date`"
+
